f5be5fa3e96dbb55c0dfd670abb7cb9ebe067090
Facundo Irala <facundoiralam@gmail.com>
2023-04-14 14:53:29 -0300

    menu con funciones

--- Clases/clase_04.py
+++ Clases/clase_04.py
@@ -1,29 +1,105 @@
-# {lista_bzrp=[    
-#     {
-#         'title': 'QUEVEDO || BZRP Music Sessions #52',
-#         'views': 227192970,
-#         'length': 204,
-#         'img_url': 'https://i.ytimg.com/vi/A_g3lMcWVy0/sddefault.jpg', 
-#         'url': 'https://youtube.com/watch?v=A_g3lMcWVy0',
-#         'date': '2022-07-06 00:00:00'
-#      }, 
-#      {
-#         'title': 'VILLANO ANTILLANO || BZRP Music Sessions #51',
-#         'views': 112947399, 
-#         'length': 188, 
-#         'img_url': 'https://i.ytimg.com/vi/wvz97-lNPH8/sddefault.jpg', 
-#         'url': 'https://youtube.com/watch?v=wvz97-lNPH8', 
-#         'date': '2022-06-08 00:00:00'
-#      }]}
+
 
 from data import lista_bzrp
 from os import system
+
+def mostrar_lista_temas():
+    # B. Recorrer la lista imprimiendo por consola el titulo de cada video
+    for i in range(len(lista_bzrp)):
+        print(f"{lista_bzrp[i]['title']}")  
+
+def mostrar_lista_temas_views():
+    # C. Recorrer la lista imprimiendo por consola el título de cada video junto a la cantidad de reproducciones del mismo
+    for tema in lista_bzrp:    
+        print (f"{tema['tema']} - {tema['views']}")
+
+def buscar_temas_mas_views():
+    # D. Recorrer la lista y determinar cuál es la cantidad máxima de reproducciones
+    flag_primero = True
+
+    for tema in lista_bzrp:
+        if flag_primero == True or tema ['views'] > maximo_views:
+            maximo_views = tema["views"]
+            flag_primero = False
+
+    print (f"Cantidad maxima de reproduccion: {maximo_views}") # si hay mas de 2 canciones iguales con misma cantidad de viewers
+    for tema in lista_bzrp:
+            if tema["views"] == maximo_views:
+                print(f"{tema['title']}")
+
+def buscar_temas_menos_views():
+    # E. Recorrer la lista y determinar cuál es la cantidad mínima de reproducciones (MÍNIMO)
+    for tema in lista_bzrp:
+        if flag_primero == True or  tema["views"] < minima_views:
+            minima_views = tema ["views"]
+            flag_primero = False
+        
+        print (f"Cantidad minima de reproduccion: {minima_views}") # si hay mas de 2 canciones iguales con misma cantidad de viewers
+        for tema in lista_bzrp:
+            if tema["views"] == minima_views:
+                print(f"{tema['title']}")
+
+def calcular_promedio():
+    # F. Recorrer la lista y determinar la cantidad total de reproducciones del canal (ACUMULADOR)
+    acumulador_views = 0
+    for tema in lista_bzrp:
+        acumulador_views += tema["views"]
+
+    print(f"Sumatoria  de reproducciones: {acumulador_views}")
+
+    # G. Recorrer la lista y determinar la cantidad total de reproducciones del canal (ACUMULADOR)
+
+    contar_temas = len(lista_bzrp)
+
+    promedio_views = acumulador_views / contar_temas
+
+    print(f"El promedio de vistas es: {promedio_views}")
+
+def mostrar_menu():
+    for opcion in menu:
+        print(opcion)
+
+
+menu = ["1.Mostrar temas", "2.Mostrar temas con vistas","3.Mostrar temas mas vistas","4.Mostrar temas con  menos views","5.Mostrar promedio reproducciones","6.Salir"]
+
+seguir = True
+while seguir == True:
+    system("cls") # eliminar buffer
+    mostrar_menu()
+
+
+    respuesta = int(input("Ingrese una opcion"))
+    match respuesta:
+        case 1:
+            mostrar_lista_temas()
+            #for tema in lista_bzrp:
+                #print (tema["title"]) 
+        case 2: 
+            mostrar_lista_temas_views()
+        case 3:
+            buscar_temas_mas_views()
+        case 4:
+            buscar_temas_menos_views()
+        case 5:
+            calcular_promedio()
+        case 6:
+            seguir = False
 
 
 # print(type(lista_bzrp))
 # print(type(lista_bzrp[0]))
 
 
-# B. Recorrer la lista imprimiendo por consola el titulo de cada video
-for tema in lista_bzrp:
-    print (tema["title"])
+
+
+# otra manera
+#  contar_temas = 0
+
+#     for tema in lista_bzrp:
+#         contar_temas += 1
+
+
+# contar_temas = len(lista_bzrp) #no espepecifica algo en particular, es generico
+
+
+# H. Informar cual es el Título del vídeo asociado a cada uno de los indicadores anteriores (MÁXIMO, MÍNIMO, ACUMULADOR y PROMEDIO)
---
+++ Clases/ejercicio_marvel.py
@@ -0,0 +1,143 @@
+from data_stark import lista_personajes
+from os import system
+
+
+def mostrar_lista_superheroes():
+     #B. Recorrer la lista imprimiendo por consola el nombre de cada superhéroe
+    for i in range(len(lista_personajes)):
+        print(f"{lista_personajes[i]['nombre']}")  
+
+def mostrar_lista_superheroes_altura():
+     #C. Recorrer la lista imprimiendo por consola nombre de cada superhéroe junto a la altura del mismo
+    for nombre in lista_personajes:    
+        print (f"{nombre['nombre']} - {nombre['altura']}")
+
+def mostrar_superheroe_mas_alto():
+     #D. Recorrer la lista y determinar cuál es el superhéroe más alto (MÁXIMO)
+
+    flag_primero = True
+
+    for personaje in lista_personajes:
+        if flag_primero == True or float(personaje ['altura'])> maximo_altura:
+            maximo_altura = float(personaje["altura"])
+            nombre_max_altura = personaje ['nombre']
+            flag_primero = False
+
+    print (f"La altura maxima del superheroe: {maximo_altura} y su nombre es: {nombre_max_altura}") 
+    for personaje in lista_personajes:
+            if float(personaje["altura"]) == maximo_altura:
+                print(f"{personaje['nombre']}")
+
+def mostrar_superheroe_menos_alto():
+     #E. Recorrer la lista y determinar cuál es el superhéroe más bajo (MÍNIMO)
+    flag_primero = True
+
+    for personaje in lista_personajes:
+        if flag_primero == True or float(personaje ['altura']) < minima_altura:
+            minima_altura = float(personaje["altura"])
+            nombre_max_altura = personaje ['nombre']
+            flag_primero = False
+
+    print (f"La altura minima del superheroe: {minima_altura} y su nombre es: {nombre_max_altura}") 
+
+    for personaje in lista_personajes:
+            if float(personaje["altura"]) == minima_altura:
+                print(f"{personaje['nombre']}")
+
+def calcular_promedio_altura_superheroes():
+     #F. Recorrer la lista y determinar la altura promedio de los superhéroes (PROMEDIO)
+
+    acumulador_altura = 0
+    for tema in lista_personajes:
+        acumulador_altura += float(tema["altura"])
+
+    contar_heroes = len(lista_personajes)
+
+    promedio_altura = (float(acumulador_altura)) / contar_heroes
+
+    print(f"El promedio de altura de los superheroes es: {promedio_altura}")
+
+def mostrar_superheroe_mas_peso():
+     # H. Calcular e informar cual es el superhéroe más y menos pesado.
+    flag_primero = True
+
+    for personaje in lista_personajes:
+        if flag_primero == True or float(personaje ['peso'])> mayor_peso:
+            mayor_peso = float(personaje["peso"])
+            nombre_max_altura = personaje ['nombre']
+            flag_primero = False
+
+    print (f"El peso del superheroes mas pesado es: {mayor_peso} y su nombre es: {nombre_max_altura}") 
+
+    for personaje in lista_personajes:
+            if float(personaje["peso"]) == mayor_peso:
+                print(f"{personaje['nombre']}")
+
+
+def mostrar_superheroe_menos_peso():
+     # H. Calcular e informar cual es el superhéroe más y menos pesado.
+    flag_primero = True
+    
+    for personaje in lista_personajes:
+        if flag_primero == True or float(personaje ['peso']) < menor_peso:
+            menor_peso = float(personaje["peso"])
+            nombre_max_altura = personaje ['nombre']
+            flag_primero = False
+
+    print (f"El peso del superheroes menos pesado es: {menor_peso} y su nombre es: {nombre_max_altura}") 
+
+    for personaje in lista_personajes:
+            if float(personaje["peso"]) == menor_peso:
+                print(f"{personaje['nombre']}")
+
+
+def mostrar_menu():
+    for opcion in menu:
+        print(opcion)
+        
+     
+
+menu =  ["1.Mostrar lista superheroes", 
+        "2.Mostrar lista superheroes y altura",
+        "3.Mostrar superheroe mas alto",
+        "4.Mostrar superheroe menos alto",
+        "5.Calcular promedio altura de superheroes",
+        "6.Mostrar superheroe con mas peso",
+        "7.Mostrar superheroe con menos peso",
+        "8.Salir"
+        ]
+
+
+seguir = True
+
+while seguir == True:
+    mostrar_menu()
+     
+    respuesta = int(input("Ingrese una opcion"))
+
+    match respuesta:
+        case 1:
+            mostrar_lista_superheroes()
+        case 2: 
+            mostrar_lista_superheroes_altura()
+        case 3:
+            mostrar_superheroe_mas_alto() 
+        case 4:
+            mostrar_superheroe_menos_alto()    
+        case 5:
+            calcular_promedio_altura_superheroes()
+        case 6:
+            mostrar_superheroe_mas_peso()
+        case 7:
+            mostrar_superheroe_menos_peso()
+        case 8:
+            mostrar_super_alto_bajo()
+        case other:
+            print("Dato incorrecto")
+
+
+
+# mostrar nombres super en case
+# crear la variable pa esto 
+
+# asignar a cada funcion flag igual true
